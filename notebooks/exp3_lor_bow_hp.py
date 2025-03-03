29c13cc508940628369ced7f0c4ee69c95722579
Upesh <upeshjeengar@gmail.com>
2025-03-03 21:10:12 +0530

    commit

--- notebooks/exp3_lor_bow_hp.py
+++ notebooks/exp3_lor_bow_hp.py
@@ -21,8 +21,8 @@ warnings.filterwarnings("ignore")
 # os.environ["MLFLOW_DISABLE_ARTIFACTS_DOWNLOAD"] = "1"
 
 # Set MLflow Tracking URI & DAGsHub integration
-MLFLOW_TRACKING_URI = "https://dagshub.com/vikashdas770/YT-Capstone-Project.mlflow"
-dagshub.init(repo_owner="vikashdas770", repo_name="YT-Capstone-Project", mlflow=True)
+MLFLOW_TRACKING_URI = "https://dagshub.com/Upeshjeengar/MLOps-project.mlflow"
+dagshub.init(repo_owner="upeshjeengar", repo_name="MLOps-Project", mlflow=True)
 mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 mlflow.set_experiment("LoR Hyperparameter Tuning")
 
